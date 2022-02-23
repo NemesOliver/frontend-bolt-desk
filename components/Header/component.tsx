a8323dd17646111282ae5c4074a5aad9d1b170d7
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2022-02-23 17:06:50 +0000

    fix: Header styles

--- components/Header/component.tsx
+++ components/Header/component.tsx
@@ -5,12 +5,14 @@ import { Logo } from "./libs";
 
 export const Header: FC = () => {
   return (
-    <header className="bg-background shadow-md h-10 flex items-center justify-between">
-      <div className="w-20 h-10">
-        <Logo />
+    <header className="bg-background shadow-md h-10 ">
+      <div className="flex items-center justify-between max-w-[1440px] mx-auto pr-2">
+        <div className="w-20 h-10">
+          <Logo />
+        </div>
+        {/* <p> tag will show user */}
+        <p>John Doe</p>
       </div>
-      {/* <p> tag will show user */}
-      <p>John Doe</p>
     </header>
   );
 };
